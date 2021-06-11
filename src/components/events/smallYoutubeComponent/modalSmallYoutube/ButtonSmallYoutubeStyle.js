9915d608fd9c0f562efc4a18059a7b37e184cc46
jperezviloria <protobot.dev@gmail.com>
2021-06-10 23:30:37 -0300

    adding new modals to public events

---
+++ src/components/events/smallYoutubeComponent/modalSmallYoutube/ButtonSmallYoutubeStyle.js
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+
+export const Boton = styled.button`
+    border-radius: 20px;
+    border: 4px solid #4275a5;
+    padding: 6px 15px;
+    
+
+
+    font-weight: 900;
+  @media only screen and (min-width: 310px) {
+    padding: 10px 50px;
+    /* margin-top:0px;
+    margin-bottom: 0px; */
+  }
+  @media only screen and (min-width: 480px) {
+    padding: 10px 100px;
+    /* margin-top:0px;
+    margin-bottom: 0px; */
+  }
+  @media only screen and (min-width: 768px) {
+    
+    padding: 0px 0px;
+    border-radius: 100px;
+    
+    }
+`
