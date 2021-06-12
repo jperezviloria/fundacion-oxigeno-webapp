a632ead145fd230a27273c55465303d711cfc17e
jperezviloria <protobot.dev@gmail.com>
2021-06-12 20:49:07 -0300

    adding responsive style to public routes

--- src/components/events/mainYoutubeComponent/MainYoutubeComponent.jsx
+++ src/components/events/mainYoutubeComponent/MainYoutubeComponent.jsx
@@ -4,7 +4,7 @@ import "./MainYoutubeComponent.css"
 
 const MailYoutubeComponent = ({indexEvent, indexYoutubeLink, particularYoutubeLink, decideMainYoutubeLink}) =>{
     return (
-    <div>
+    <div className="main-youtube">
         <h1>{particularYoutubeLink.name}</h1>
         <iframe className="main-youtube-iframe"  src={particularYoutubeLink.link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         
